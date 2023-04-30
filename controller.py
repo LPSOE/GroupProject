58b736786f2ede546c2a337f6965c455cea6081d
LPSOE <96891549+LPSOE@users.noreply.github.com>
2023-04-30 07:28:28 -0500

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,7 @@
+from view import *
+from PyQt5.QtWidgets import *
+
+
+class Controller(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
