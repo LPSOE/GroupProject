f93dd31bb4141368c87f1268580f7c51cc8466fb
LPSOE <96891549+LPSOE@users.noreply.github.com>
2023-04-30 07:21:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#Hello World
+from PyQt5.QtWidgets import *
+from controller import *
+
+def main():
+    app = QApplication([])
+    window = Controller()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
