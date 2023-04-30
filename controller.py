2afbe23a0cfde0c08a3b6705c021dad3b71d4288
LPSOE <96891549+LPSOE@users.noreply.github.com>
2023-04-30 07:48:57 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -5,3 +5,4 @@ from PyQt5.QtWidgets import *
 class Controller(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.setupUi(self)
